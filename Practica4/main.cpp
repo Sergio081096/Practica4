4f85d00b2a6433010d7c6e9e4645f8bbced8d811
Sergio081096 <31215551+Sergio081096@users.noreply.github.com>
2017-09-08 17:06:58 -0500

    Final

--- Practica4/main.cpp
+++ Practica4/main.cpp
@@ -11,6 +11,10 @@
 
 float transZ = -5.0f;
 float transX = -5.0f;
+float transY = -5.0f;
+float angleX = 0.0f;
+float angleY = 0.0f;
+float angleZ = 0.0f;
 int screenW = 0.0;
 int screenH = 0.0;
 
@@ -96,24 +100,92 @@ void display ( void )   // Creamos la funcion donde se dibuja
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-		glTranslatef(0.0f, 0.0f, transZ);
+		glTranslatef(0.0+transX,0.0+transY,-5.0+transZ);
+		glRotatef(angleX,8.0,0.0,0.0);
+		glRotatef(angleY,0.0,8.0,0.0);
+		glRotatef(angleZ,0.0,0.0,8.0);
 		//Poner Código Aquí.
 		glRotatef(transX,0.0,1.0,0.0);
 
 		//glTranslatef(0.0,0.0,0.0);
-		//cabeza
+		//nariz
 		glPushMatrix();	
 		glTranslatef(0.0,0.0,1.0);
-			glColor3f(1.0,0.0,0.0);
+			glColor3f(1.0,1.0,0.2);
 			prisma();//0,0
 		glPopMatrix();	
-		//cuello
+		//cabeza
 		glPushMatrix();	
-			glColor3f(0.0,1.0,0.0);
-			glTranslatef(0.0,-0.75,1.0);
-			glScalef(0.5,0.5,1.0);
+			glColor3f(1.0,1.0,0.0);
+			glTranslatef(1.1,0.5,1.0);
+			glScalef(1.2,2.0,1.8);
 			prisma();
 		glPopMatrix();
+		//oreja izquierda
+		glPushMatrix();	
+			glColor3f(1.0,0.0,0.0);
+			glTranslatef(1.5,1.8,1.6);
+			glScalef(0.4,0.6,0.6);
+			prisma();
+		glPopMatrix();
+		//oreja derecha
+		glPushMatrix();	
+			glColor3f(1.0,0.0,0.0);
+			glTranslatef(1.5,1.8,0.4);
+			glScalef(0.4,0.6,0.6);
+			prisma();
+		glPopMatrix();
+		//medio cuerpo
+		glPushMatrix();	
+			glColor3f(1.0,1.0,1.0);
+			glTranslatef(2.95,0.5,1.0);
+			glScalef(2.5,2.4,2.4);
+			prisma();
+		glPopMatrix();
+		//otra mitad
+		glPushMatrix();	
+			glColor3f(0.0,1.0,0.0);
+			glTranslatef(5.7,0.5,1.0);
+			glScalef(3.0,2.0,1.8);
+			prisma();
+		glPopMatrix();
+		//pierna delantera izquierda
+		glPushMatrix();	
+			glColor3f(1.0,1.0,0.0);
+			glTranslatef(2.4,-1.9,1.7);
+			glScalef(0.6,2.4,0.6);
+			prisma();
+		glPopMatrix();		
+		//pierna delantera derecha
+		glPushMatrix();	
+			glColor3f(1.0,1.0,0.0);
+			glTranslatef(2.4,-1.9,0.3);
+			glScalef(0.6,2.4,0.6);
+			prisma();
+		glPopMatrix();
+		//pierna trazera izquierda
+		glPushMatrix();	
+			glColor3f(1.0,1.0,0.0);
+			glTranslatef(6.5,-1.8,0.4);
+			glScalef(0.6,2.6,0.6);
+			prisma();
+		glPopMatrix();
+		//pierna trazera derecha
+		glPushMatrix();	
+			glColor3f(1.0,1.0,0.0);
+			glTranslatef(6.5,-1.8,1.6);
+			glScalef(0.6,2.6,0.6);
+			prisma();
+		glPopMatrix();
+		//cola
+		glPushMatrix();	
+			//glRotatef(45,1.0,0.0,0.0);//angulo-ejes
+			glColor3f(1.0,1.0,0.0);
+			glTranslatef(7.9,0.5,1.0);
+			glScalef(1.4,0.6,0.6);
+			prisma();
+		glPopMatrix();
+		/*
 		//torso
 		glPushMatrix();	
 			glColor3f(1.0,1.0,1.0);
@@ -209,7 +281,7 @@ void reshape ( int width , int height )   // Creamos funcion Reshape
 
 	// Tipo de Vista
 	//glOrtho(-5,5,-5,5,0.2,20);	
-	glFrustum (-0.1, 0.1,-0.1, 0.1, 0.1, 50.0);
+	glFrustum (-0.1, 0.1,-0.1, 0.1, 0.1, 30.0);
 
 	glMatrixMode(GL_MODELVIEW);							// Seleccionamos Modelview Matrix
 }
@@ -219,23 +291,43 @@ void keyboard ( unsigned char key, int x, int y )  // Create Keyboard Function
 	switch ( key ) {
 		case 'w':
 		case 'W':
-			transZ +=0.3f;
+			transZ +=0.2f;
+			printf("Posicion en Z: %f\n", transZ);
 			break;
 		case 's':
 		case 'S':
-			transZ -= 0.3f;
+			transZ -=0.2f;
+			printf("Posicion en Z: %f\n", transZ);
 			break;
 		case 'a':
 		case 'A':
 			printf("Posicion en X: %f\n", transX);
-			transX -=0.5f;
+			transX -=0.2f;
 			break;
 		case 'd':
 		case 'D':
 			printf("Posicion en X: %f\n", transX);
-			transX +=0.5f;
+			transX +=0.2f;
 			break;
-	
+		case 'q':
+		case 'Q':
+			printf("Posicion en Y: %f\n", transY);
+			transY +=0.2f;
+			break;
+		case 'e':
+		case 'E':
+			printf("Posicion en Y: %f\n", transY);
+			transY -=0.2f;
+			break;
+		case 't':
+		case 'T':
+			printf("Posicion en Angulo: %f\n", transY);
+			angleZ +=1.2f;
+			break;
+		case 'g':
+		case 'G':
+			printf("Posicion en Angulo: %f\n", transY);
+			angleZ -=1.2f;
 			break;
 		case 27:        // Cuando Esc es presionado...
 			exit ( 0 );   // Salimos del programa
@@ -249,13 +341,21 @@ void keyboard ( unsigned char key, int x, int y )  // Create Keyboard Function
 void arrow_keys ( int a_keys, int x, int y )  // Funcion para manejo de teclas especiales (arrow keys)
 {
   switch ( a_keys ) {
-    case GLUT_KEY_UP:		// Presionamos tecla ARRIBA...
+   case GLUT_KEY_UP:     // Presionamos tecla ARRIBA...
+		angleX += 1.0f;
 		break;
-    case GLUT_KEY_DOWN:		// Presionamos tecla ABAJO...
+      //glutFullScreen ( ); // Full Screen Mode
+      //break;
+    case GLUT_KEY_DOWN:               // Presionamos tecla ABAJO...
+		angleX -= 1.0f;
 		break;
+      //glutReshapeWindow ( 500, 500 ); // Cambiamos tamano de Ventana
+      //break;
 	case GLUT_KEY_LEFT:
+		angleY += 1.0f;
 		break;
 	case GLUT_KEY_RIGHT:
+		angleY -= 1.0f;
 		break;
     default:
       break;
@@ -272,7 +372,7 @@ int main ( int argc, char** argv )   // Main Function
   screenW = glutGet(GLUT_SCREEN_WIDTH);
   screenH = glutGet(GLUT_SCREEN_HEIGHT);
   glutInitWindowSize  (500, 500);	// Tamaño de la Ventana
-  glutInitWindowPosition (0, 0);	//Posicion de la Ventana
+  glutInitWindowPosition (0,-10);	//Posicion de la Ventana
   glutCreateWindow    ("Practica 4"); // Nombre de la Ventana
   printf("Resolution H: %i \n", screenW);
   printf("Resolution V: %i \n", screenH);
